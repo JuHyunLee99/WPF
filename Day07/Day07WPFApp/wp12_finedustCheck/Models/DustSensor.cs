d233386bddf35892308300f5ee01fcf70c88d811
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-04-27 17:28:24 +0900

    커밋 7일차 끝

---
+++ Day07/Day07WPFApp/wp12_finedustCheck/Models/DustSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace wp12_finedustCheck.Models
+{
+    public class DustSensor
+    {
+        public int Id { get; set; }
+        public string Dev_id { get; set; }
+        public string Name { get; set;}
+        public string LOC { get; set;}
+        public double Coordx { get; set;}
+        public double Coordy { get; set;}
+        public bool Ison { get; set;}
+        public int Pm10_after { get; set;}
+        public int Pm25_after { get; set;}
+        public int State { get;set;}
+        public DateTime Timestamp { get; set; }
+        public string Company_id { get; set;}
+        public string Company_name { get; set; }
+
+    }
+}
