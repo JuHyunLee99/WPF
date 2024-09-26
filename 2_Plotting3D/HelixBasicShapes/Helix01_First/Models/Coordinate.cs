56eba92a427fda370bdfc710518ef1d8883d8a9f
JuHyunLee99 <juhyun.lee0829@daum.net>
2024-09-26 17:38:57 +0900

    mvvc패턴

---
+++ 2_Plotting3D/HelixBasicShapes/Helix01_First/Models/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helix01_First.Models
+{
+    public class Coordinate : ObservableObject
+    {
+        private double x; private double y; private double z; private double tx; private double ty; private double tz;
+        public double X { get { return x; } set { SetProperty(ref x, value); } }
+        public double Y { get { return y; } set { SetProperty(ref y, value); } }
+        public double Z { get { return z; } set { SetProperty(ref z, value); } }
+        public double TX { get { return tx; } set { SetProperty(ref tx, value); } }
+        public double TY { get { return ty; } set { SetProperty(ref ty, value); } }
+        public double TZ { get { return tz; } set { SetProperty(ref tz, value); } }
+    }
+}
---
+++ 2_Plotting3D/HelixBasicShapes/Helix01_First/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Helix01_First.Models;
+using Helix01_First.ViewModels.Base;
+using System.Collections.Generic;
+
+namespace Helix01_First.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private List<Coordinate> _head;
+        private List<Coordinate>  _prob;
+
+        public List<Coordinate> Head
+        {
+            get => _head;
+            set => SetProperty(ref _head, value);
+        }
+        public List<Coordinate> Prob
+        {
+            get => _prob;
+            set => SetProperty(ref _prob, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Head = new List<Coordinate> { new Coordinate { X = 0, Y = 0, Z = 0, TX = 0, TY = 0, TZ = 0 } };
+            Prob = new List<Coordinate>();
+        }
+
+    }
+}
